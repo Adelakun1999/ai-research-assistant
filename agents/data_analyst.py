19ba994f91d868d972eb7def5a6c4617b754ee9d
Adelakun Yusuf Adekunle <kingdomagent1999@gmail.com>
2025-10-08 08:57:10 +0100

    agent

---
+++ agents/data_analyst.py
@@ -0,0 +1,21 @@
+import os 
+from crewai import Agent , LLM
+from crewai_tools import FileReadTool
+
+model = os.getenv("ANALYST_AGENT_LLM")
+temperature = float(os.getenv("ANALYST_AGENT_TEMPERATURE"))
+
+llm = LLM(model=model, temperature=temperature)
+
+data_analyst_agent = Agent(
+    role="Data Analyst",
+    goal="Analyze gathered information to extract key insights, patterns, and conclusions",
+    backstory = (
+                "You are a skilled data analyst with expertise in synthesizing complex "
+                "information into actionable insights. You excel at identifying patterns, trends, "
+                "and key findings from research data."
+            ),
+    llm=llm,
+    tools=[FileReadTool()],
+    verbose=True,
+)
